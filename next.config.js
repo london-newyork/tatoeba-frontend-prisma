273c09be095188abfceef39a5cba54a435995dd0
london-newyork <cactus1272004@yahoo.co.jp>
2022-01-28 14:25:50 +0900

    TypeScriptの設定ファイルを追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   reactStrictMode: true,
 }
- 
+
+// ESLintの設定 eslint-disable を追加する
+/* eslint-disable
+    @typescript-eslint/no-var-requires,
+    @typescript-eslint/explicit-function-return-type
+*/
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {
+  
+}
+export const Header = (props:Props) => {
+  return (
+    <header>
+      <div></div>
+    </header>
+  )
+};

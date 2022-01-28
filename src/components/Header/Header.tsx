83e955e9d243250959d8cda9e9dc53b9d6f4979f
london-newyork <cactus1272004@yahoo.co.jp>
2022-01-28 17:07:22 +0900

    tailwindCSS設定とテスト

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 type Props = {
-  
+
 }
+
 export const Header = (props:Props) => {
   return (
     <header>
-      <div></div>
+      <div className='bg-light-green w-full h-[80px]'>
+        <>
+        tatoeba
+        </>
+      </div>
     </header>
   )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,40 @@
 module.exports = {
+  mode: 'jit',
+  purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+
+    extend: {
+      '128': '32rem',
+      '144': '36rem',
+    },
+    borderRadius: {
+      '4xl': '2rem',
+    }
+  },
+  theme: {
+    color: (theme) => ({
+      ...theme("colors"),
+      'dark-green' :'#05D200',
+    }),
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      'light-green': '#D2FDD1',
+      'dark-green' :'#05D200',
+    }),
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
   plugins: [],
+  variants: {
+    extend: {},
+  },
 }

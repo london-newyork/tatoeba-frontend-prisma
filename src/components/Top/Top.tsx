05476b22dcaad8dd5ffd079f3b6250624177c7af
london-newyork <cactus1272004@yahoo.co.jp>
2022-03-06 23:59:01 +0900

    WIP query過去の分と現在の分を連想配列化

--- src/components/Top/Top.tsx
+++ src/components/Top/Top.tsx
@@ -1,37 +1,69 @@
-import React, { VFC, useState, useEffect } from 'react';
+import React, { VFC, useState, useEffect, useRef, SetStateAction } from 'react';
 import { useRouter } from 'next/router';
 import { TopUpperContents } from '../Top/TopUpperContents'
+import { ParsedUrlQuery } from 'node:querystring'
 
 export const Top:VFC = () => {
     const router = useRouter();
-    const [formQuery, setFormQuery]:any = useState([router.query])
-    // const [formQuery, setFormQuery]:any = useState([])
+    const [routerQuery, setRouterQuery] = useState([router.query])
 
     // const [title, setTitle ] = useState('')
     // const [shortParaphrase, setShortParaphrase ] = useState('')
     // const [description, setDescription] = useState('')
-    const id = router.query.id //登録ページから今来たidをidに格納
-    const title = router.query.title
-    const shortParaphrase = router.query.shortParaphrase
-    const description = router.query.description
-    // const { id, title, shortParaphrase, description } = router.query
 
+    // const id = router.query.id //登録ページから今来たidをidに格納
+    // const title = router.query.title
+    // const shortParaphrase = router.query.shortParaphrase
+    // const description = router.query.description
+
+    //登録ページから来た内容を変数へ代入
+    const { id, title, shortParaphrase, description } = router.query
+
+    const renderFlgRef = useRef(false)
     //useEffectで再レンダリングが走らないようにする
     useEffect(() => {
-        return () => {
-            setFormQuery([
-                //今までの例えが格納されているはず・・
-                ...formQuery,{
-                id,//登録ページから今来たid
-                title,
-                shortParaphrase,
-                description
-                }
-            ])
-        }
-    }, [])
+        //useRefを使って、useEffectがformQueryを監視中に、２回レンダリングが走らないようにする。
+        if(renderFlgRef.current) {
 
-    console.log(formQuery) //formQuery.id だとundefined => idなどがちゃんとformQueryに収納されていない。
+            //登録ページからのidと、formQueryに格納されたidが同じかどうか判定するためにidをmapで取り出す
+            // const newRouterQuery =
+            // routerQuery.map((query:any)=> {
+            //     return query.id
+            // })
+
+            //登録ページからのidと、formQueryに格納されたidが同じかどうか判定
+            // id === newRouterQuery
+            // ? setRouterQuery(prevRouterQuery => {
+            //     const newRouterQuery =
+            //     //今までの例えが格納されているはず・・
+            //     [ ...prevRouterQuery, { 'a':'aaa' }]
+            //     return newRouterQuery
+            // })
+            // : null
+
+            //登録ページからのidと、formQueryに格納されたidが同じかどうか判定
+
+            setRouterQuery(prevRouterQuery => {
+                const newRouterQuery =
+                //今までの例えが格納されているはず・・
+                [ ...prevRouterQuery, { id, title, shortParaphrase, description }]
+                return newRouterQuery
+            })
+
+        } else {
+            renderFlgRef.current = true
+        }
+    }, [routerQuery])
+
+    console.log(routerQuery)
+    //[{...}]//オブジェクトに値は入ってるが、もう一度違うものを登録してみるとまた別の配列に入っている。
+
+    //[{...}]
+    //[{...}]
+    //[{...}]
+
+    // [{0:id...  1:id... 2:id...}] <=こういう状態にしたい。
+
 /////////////////////////////////////////////////
 
 // => firebaseにデータを入れてしまうので、Recoilも状態管理も関係ない。
@@ -63,10 +95,10 @@ export const Top:VFC = () => {
                 gap-x-6
                 gap-y-8
                 mt-4'>
-                {formQuery
-                ? formQuery.map((query:any) => (
+                {routerQuery
+                ? routerQuery.map((item:any) => (
                     <li
-                    key={query.id}
+                    key={item.id}
                     className='
                     px-6
                     py-10
@@ -79,7 +111,7 @@ export const Top:VFC = () => {
                     '>
                         <h3 className='
                         text-lg
-                        text-center'>{query.title}を例えると...{query.shortParaphrase}</h3>
+                        text-center'>{item.title}を例えると...{item.shortParaphrase}</h3>
                         <ul className='
                         pt-9
                         '>
@@ -89,7 +121,7 @@ export const Top:VFC = () => {
                             items-center'>
                                 <img src="" alt="" className='w-56 h-36'/>
                             </li>
-                            <li>{query.description}</li>
+                            <li>{item.description}</li>
                         </ul>
                     </li>
                 )

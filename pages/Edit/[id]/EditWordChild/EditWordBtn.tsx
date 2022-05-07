9b4ca048befa4af5cb85db8f19ba01dc807a9023
london-newyork <cactus1272004@yahoo.co.jp>
2022-05-07 13:49:49 +0900

    使っていない旧Editページを一旦コメントアウト - Recoil atomに格納しているwordsのtIdの値が1つのリストに異常に格納されていたり、キャッシュをリフレッシュしているのに 最初からnullが37回もwordsの中に入っているため、疑わしい部分を排除したい。そのためwordsを使うページをコメントアウトした。 - ただしどこかのタイミングで参照する可能性があり、削除せず残しておく。

--- pages/Edit/[id]/EditWordChild/EditWordBtn.tsx
+++ pages/Edit/[id]/EditWordChild/EditWordBtn.tsx
@@ -1,72 +1,72 @@
-import React, {useState} from 'react'
-import { Modal } from '../../../../src/components/Modal/Modal'
-import { useRouter } from 'next/router';
-import { useRecoilState } from 'recoil';
-import { WordsAtom } from '../../../../src/components/utils/atoms/WordsAtom'
+// import React, {useState} from 'react'
+// import { Modal } from '../../../../src/components/Modal/Modal'
+// import { useRouter } from 'next/router';
+// import { useRecoilState } from 'recoil';
+// import { WordsAtom } from '../../../../src/components/utils/atoms/WordsAtom'
 
-export const EditWordBtn = ({tId, title, shortParaphrase, description, creationTime}) => {
+// export const EditWordBtn = ({tId, title, shortParaphrase, description, creationTime}) => {
 
-  const [words, setWords] = useRecoilState(WordsAtom)
-  const [show, setShow] = useState(false)
+//   const [words, setWords] = useRecoilState(WordsAtom)
+//   const [show, setShow] = useState(false)
 
-  const router = useRouter()
+//   const router = useRouter()
 
-  // const openModal = useCallback(() => {
-  //   setShow(true)
-  // }, [])
+//   // const openModal = useCallback(() => {
+//   //   setShow(true)
+//   // }, [])
 
-  // function getUniqueId(){
-  //   return new Date().getTime().toString(36) + '-' + Math.random().toString(36)
-  // }
-  // const tId = getUniqueId()
+//   // function getUniqueId(){
+//   //   return new Date().getTime().toString(36) + '-' + Math.random().toString(36)
+//   // }
+//   // const tId = getUniqueId()
 
-  const submitWords = (e) => {
-    {/* @ts-ignore */}
-    // setShow(false)
+//   const submitWords = (e) => {
+//     {/* @ts-ignore */}
+//     // setShow(false)
 
-    const newWords = [
-      {
-        tId,
-        title,
-        shortParaphrase,
-        description,
-        creationTime
-      },
-      ...words,
-    ]
-    setWords(newWords)
+//     const newWords = [
+//       {
+//         tId,
+//         title,
+//         shortParaphrase,
+//         description,
+//         creationTime
+//       },
+//       ...words,
+//     ]
+//     setWords(newWords)
 
-    router.push({
-      pathname:'/',
-      query: {
-        tId,
-        title,
-        shortParaphrase,
-        description,
-        creationTime
-      }
-    })
-}
+//     router.push({
+//       pathname:'/',
+//       query: {
+//         tId,
+//         title,
+//         shortParaphrase,
+//         description,
+//         creationTime
+//       }
+//     })
+// }
 
-  return (
-    <div className="flex justify-end">
-    <button
-    // onClick={openModal}
-      onClick={submitWords}
-      type="submit"
-      className="
-      p-3
-      w-[200px]
-      rounded-md
-      bg-dark_green
-      text-white
-      text-lg
-      hover:bg-opacity-90
-    ">
-      更新する
-    </button>
-    {/* @ts-ignore */}
-    {/* <Modal show={show} setShow={setShow}/> */}
-  </div>
-  )
-}
+//   return (
+//     <div className="flex justify-end">
+//     <button
+//     // onClick={openModal}
+//       onClick={submitWords}
+//       type="submit"
+//       className="
+//       p-3
+//       w-[200px]
+//       rounded-md
+//       bg-dark_green
+//       text-white
+//       text-lg
+//       hover:bg-opacity-90
+//     ">
+//       更新する
+//     </button>
+//     {/* @ts-ignore */}
+//     {/* <Modal show={show} setShow={setShow}/> */}
+//   </div>
+//   )
+// }

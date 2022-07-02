e5dd67da34c9f7ca6a2586dea8c1958cb28adb46
YI <57102971+london-newyork@users.noreply.github.com>
2022-07-02 23:34:14 +0900

    #3 例え一覧ボタンの設置

---
+++ src/components/btn/UserTatoeListBtn.tsx
@@ -0,0 +1,24 @@
+export const UserTatoeListBtn = () => {
+  return (
+    <button
+    className='
+    text-white
+    fixed
+    text-2xl
+    left-5
+    top-40
+    '
+    >
+      <svg
+      xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="1"
+      aria-describedby='tatoelist'
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+      </svg>
+    </button>
+
+  ) }

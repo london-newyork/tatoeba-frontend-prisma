621bf36cd13f631bd668253b0e269385b679a921
YI <57102971+london-newyork@users.noreply.github.com>
2022-07-02 15:18:55 +0900

    #4 warningで指摘されているので、ケバブケースからキャメルケースへ変更 - ハイドレーションエラーの原因の一つになっていそうだったので変更

--- src/components/Layouts/DashBoard/TatoeListLayouts.tsx
+++ src/components/Layouts/DashBoard/TatoeListLayouts.tsx
@@ -43,8 +43,8 @@ export const TatoeListLayouts:VFC<Layouts> = (props) => {
             bg-dark_green
             rounded-full'
             >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-800 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path strokeLinecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-800 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
                     </svg>
                 </button>
             </Link>

3a377d763aae560a51f9bf4ce0e862e8a5b12771
YI <57102971+london-newyork@users.noreply.github.com>
2022-07-09 23:03:49 +0900

    #11 仮登録された後に表示される仮登録完了ページの作成をした - 実際にメールが登録された後にページが表示されるか確認済み

---
+++ pages/RegistrationMember/CompleteTempRegistrationMember.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import React from 'react';
+import { Header } from '../../src/components/Header/Header';
+
+const CompleteTempRegistrationMember = () => {
+  return (
+    <>
+    <Head>
+        <title>新規仮会員登録完了</title>
+        <link rel='favicon.ico' />
+    </Head>
+    <Header/>
+      <div
+      className="
+      w-full
+      pt-40
+      px-10
+      ">
+        <p
+        className='
+        flex
+        justify-center
+        leading-relaxed
+        '
+        >
+          仮登録が完了しました。<br/>
+          システムより仮登録完了のメールが届く予定ですので、お待ちくださいませ。
+
+        </p>
+      </div>
+      </>
+      );
+    };
+
+export default CompleteTempRegistrationMember;

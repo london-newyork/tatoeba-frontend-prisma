f93f01249c256a4e5e34ceeaf24c22e7cfcaef4c
YI <57102971+london-newyork@users.noreply.github.com>
2022-07-26 22:44:21 +0900

    #28 整理とuserデータの準備

--- src/components/DashBoard/Profile.tsx
+++ src/components/DashBoard/Profile.tsx
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
+import { UsersIcon } from '@heroicons/react/outline'
+import React, { useEffect, useState } from 'react'
+import { getStorage } from '../../lib/storage'
 import { ProfileImage } from './ProfileImage'
 
 type ProfileVal = string | undefined
 
 export const Profile = (props:any) => {
-    // const { userInfo } = props
-    const [ email, setEmail ] = useState<ProfileVal>()
-    const [ password, setPassword ] = useState<ProfileVal>()
-    const [ userName, setUserName ] = useState<ProfileVal>()
-    const handleChangePassword = (
-        e: React.ChangeEvent<HTMLInputElement> | undefined
-        ):void => {
-            const existingPassword = "" //DBからユーザーのデータをここに送る
-            if(!password){
-                setPassword(existingPassword)
+    // バックエンドに対してアクセストークンを渡してユーザー一覧を要求
+    // 汎用性を考えると関数名がこれでいいかはわからない。
+    useEffect(() => {
+        const sendAuthUsersAccessToken = async() => {
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users`, {
+            headers: {
+                Authorization: `Bearer ${getStorage('jwt')}`
             }
-            setPassword(e.target.value)
-    }
-    const handleChangeEmail = (
-        e: React.ChangeEvent<HTMLInputElement> | undefined
-        ):void => {
-            const existingEmail = "" //DBからユーザーのデータをここに送る
-            if(!email){
-                setEmail(existingEmail)
-            }
-            setEmail(e.target.value)
-    }
+        })
+        await response.json()
+        }
+        sendAuthUsersAccessToken()
+    },[])
+
+    // emailとuserNameをapiのデータから取り出して表示(passwordは表示しない)
+
+    const [ currentEmail, setCurrentEmail ] = useState<ProfileVal>('')
+    const [ userName, setUserName ] = useState<ProfileVal>('')
+
     const handleChangeUserName = (
         e: React.ChangeEvent<HTMLInputElement> | undefined
         ):void  => {
-            const existingUserName = "" //DBからユーザーのデータをここに送る
+            const currentUserName = "" //DBからユーザーのデータをここに送る
             if(!userName){
-                setUserName(existingUserName)
+                setUserName(currentUserName)
             }
             setUserName(e.target.value)
     }
@@ -82,7 +82,9 @@ export const Profile = (props:any) => {
                                 '>
                                     <input
                                     value={userName}
-                                    onChange={handleChangeUserName}>
+                                    onChange={handleChangeUserName}
+                                    placeholder='Nola Stradford'
+                                    >
                                     </input>
                             </h1>
 
@@ -115,27 +117,33 @@ export const Profile = (props:any) => {
                                 text-gray-400'>
                                     メールアドレス
                                 </li>
-                                <li>
-                                    <input
-                                    value={email}
-                                    className="
-                                    h-7
-                                    flex
-                                    flex-1
-                                    rounded-full
-                                    outline-none
-                                    sm:min-w-[14rem]
-                                    sm:w-[16rem]
-                                    focus:bg-gray-700
-                                    focus:text-white
-                                    p-3
-                                    "
-                                    onChange={handleChangeEmail}
-                                    />
+                                <li
+                                value={currentEmail}
+                                className="
+                                h-7
+                                flex
+                                flex-1
+                                sm:min-w-[14rem]
+                                sm:w-[20rem]
+                                w-[18rem]
+                                p-4
+                                m-2
+                                "
+                                >
                                 </li>
                             </ul>
                         </li>
-                        <li>
+                        <li
+                        className='
+                        min-w-[16rem]
+                        w-full
+                        pl-3
+                        sm:pl-0
+                        flex
+                        flex-col
+                        sm:flex-row
+                        justify-between'
+                        >
                             <ul
                             className='
                             flex
@@ -146,8 +154,6 @@ export const Profile = (props:any) => {
                             justify-start'>
                                 <li
                                 className='
-                                pl-3
-                                sm:pl-0
                                 sm:w-[128px]
                                 text-sm
                                 text-gray-400
@@ -155,58 +161,28 @@ export const Profile = (props:any) => {
                                 >
                                  パスワード
                                 </li>
-                                <li>
-                                <input
-                                    value={password}
-                                    className="
-                                    h-7
-                                    flex
-                                    flex-1
-                                    rounded-full
-                                    outline-none
-                                    sm:min-w-[14rem]
-                                    sm:w-[16rem]
-                                    focus:bg-gray-700
-                                    focus:text-white
-                                    p-3
-                                    "
-                                    onChange={handleChangePassword}
-                                    />
+                                <li
+                                className='
+                                sm:pl-4
+                                pl-2
+                                m-2
+                                text-gray-400
+                                tracking-widest
+                                '
+                                >
+                                ******
                                 </li>
                             </ul>
+                            <button
+                                className='
+                                text-gray-400
+                                '>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                    </svg>
+                            </button>
                         </li>
                 </ul>
-                <div
-                className='
-                flex
-                flex-row
-                justify-end
-                gap-x-3
-                pt-6
-                '
-                >
-                    <button
-                    className='
-                    h-8
-                    w-16
-                    rounded-full
-                    bg-dark_green
-                    text-xs
-                    '
-                    >
-                    編集
-                    </button>
-                    <button
-                    className='
-                    h-8
-                    w-16
-                    rounded-full
-                    border-dark_green
-                    border-2
-                    text-xs
-                    '
-                    >保存</button>
-                </div>
     </div>
   )
 }

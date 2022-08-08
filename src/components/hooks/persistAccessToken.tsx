c1a40ca1afacad3416a889aaaa9777f1621da4b8
YI <57102971+london-newyork@users.noreply.github.com>
2022-08-08 22:06:31 +0900

    #33 ログインをスキップするページに永続化したaccessTokenを実装中

--- src/components/hooks/persistAccessToken.tsx
+++ src/components/hooks/persistAccessToken.tsx
@@ -2,11 +2,14 @@ import { LoginUserAtom } from '../utils/atoms/LoginUserAtom';
 import { useRecoilState } from 'recoil';
 import { getStorage } from '../../lib/storage';
 import { useEffect } from 'react';
-const [accessToken, setAccessToken] = useRecoilState<string>(LoginUserAtom);
 
-useEffect(() => {
-  if (!accessToken) {
-    throw new Error();
-  }
-  setAccessToken(getStorage('jwt'));
-}, []);
+export const usePersistAccessToken = () => {
+  const [accessToken, setAccessToken] = useRecoilState<string>(LoginUserAtom);
+  useEffect(() => {
+    if (!accessToken) {
+      throw new Error();
+    }
+    setAccessToken(getStorage('jwt'));
+  }, []);
+  return { accessToken };
+};

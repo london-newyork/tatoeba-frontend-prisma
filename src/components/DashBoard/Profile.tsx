978dee7961a5585f195316f23fa98253911c9600
YI <57102971+london-newyork@users.noreply.github.com>
2022-08-12 22:57:52 +0900

    #29 [WIP] 名前を登録するための関数のベース作成

--- src/components/DashBoard/Profile.tsx
+++ src/components/DashBoard/Profile.tsx
@@ -1,193 +1,239 @@
-import { UsersIcon } from '@heroicons/react/outline'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { getStorage } from '../../lib/storage'
-import { ProfileImage } from './ProfileImage'
+import { UsersIcon } from '@heroicons/react/outline';
+import Link from 'next/link';
+import React, {
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  useEffect,
+  useState,
+} from 'react';
+import { getStorage } from '../../lib/storage';
+import { ProfileImage } from './ProfileImage';
 
-type ProfileVal = string | undefined
+type ProfileVal = string | string[] | undefined;
 
-export const Profile = (props:any) => {
-    // バックエンドに対してアクセストークンを渡してユーザー一覧を要求
-    // 汎用性を考えると関数名がこれでいいかはわからない。
-    useEffect(() => {
-        const sendAuthUsersAccessToken = async() => {
-
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users`, {
-            headers: {
-                Authorization: `Bearer ${getStorage('jwt')}`
-            }
-        })
-        await response.json()
+export const Profile = () => {
+  // バックエンドに対してアクセストークンを渡してユーザー一覧を要求
+  // 汎用性を考えると関数名がこれでいいかはわからない。
+  useEffect(() => {
+    const sendAuthUsersAccessToken = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/users`,
+        {
+          headers: {
+            Authorization: `Bearer ${getStorage('jwt')}`,
+          },
         }
-        sendAuthUsersAccessToken()
-    },[])
+      );
+      await response.json();
+    };
+    sendAuthUsersAccessToken();
+  }, []);
 
-    // emailとuserNameをapiのデータから取り出して表示(passwordは表示しない)
+  // emailとuserNameをapiのデータから取り出して表示(passwordは表示しない)
 
-    const [ currentEmail, setCurrentEmail ] = useState<ProfileVal>('')
-    const [ userName, setUserName ] = useState<ProfileVal>('')
+  // 新規会員登録した際の情報を更新する
+  const [userName, setUserName] = useState<ProfileVal>();
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+  const handleChangeUserName = (
+    e: ChangeEvent<{ value: string }> | undefined
+  ): void => {
+    setUserName(e.target.value);
+    console.log('handleChangeUserName', userName);
 
-    const handleChangeUserName = (
-        e: React.ChangeEvent<HTMLInputElement> | undefined
-        ):void  => {
-            const currentUserName = "" //DBからユーザーのデータをここに送る
-            if(!userName){
-                setUserName(currentUserName)
-            }
-            setUserName(e.target.value)
+    // 名前がない場合、投稿できないようにする => 必須
+    // 現状、投稿時に投稿者名を入れていない仕様なので追加
+  };
+
+  const handleCompositionStart = () => {
+    setIsTyping(true);
+  };
+
+  const handleCompositionEnd = () => {
+    setIsTyping(false);
+  };
+
+  // エンターキーを押され,isTypingが動作していて、漢字変換されたら名前を登録する
+  const handleOnKeyDown = (
+    event:
+      | DetailedHTMLProps<
+          InputHTMLAttributes<HTMLInputElement>,
+          HTMLInputElement
+        >
+      | undefined
+  ): void => {
+    // @ts-ignore shiftKeyの型が解決できないので一旦無視
+    if (event.key === 'Enter' && !isTyping && !event.shiftKey) {
+      //   setUserName(event.target.value);
+      //   console.log('名前変更後', userName);
     }
+  };
 
   return (
-    <div
-    className='group'
-    >
-         <div
-            className='
+    <div className='group'>
+      <div
+        className='
+        flex
+        flex-row
+        items-center
+        md:min-w-[400px]
+        sm:justify-between
+        justify-center
+        '
+      >
+        <div
+          className='
             flex
-            flex-row
             items-center
-            md:min-w-[400px]
+            flex-col
+            sm:flex-row
             sm:justify-between
             justify-center
+            gap-y-2
+            sm:gap-y-0
+            position
+            relative
             '
+        >
+          <ProfileImage />
+          <div
+            className='
+            flex
+            flex-col
+            pb-2
+            border-b
+            border-gray-200
+            sm:ml-8
+            '
+          >
+            <h1
+              className='
+                text-2xl
+                text-gray-700
+                '
             >
-                    <div
-                    className='
-                    flex
-                    items-center
-                    flex-col
-                    sm:flex-row
-                    sm:justify-between
-                    justify-center
-                    gap-y-2
-                    sm:gap-y-0
-                    position
-                    relative
-                    '
-                    >
-                        <ProfileImage />
-                        <div
-                        className='
-                        flex
-                        flex-col
-                        pb-2
-                        border-b
-                        border-gray-200
-                        sm:ml-8
-                        '
-                        >
-                            <h1
-                                className='
-                                text-2xl
-                                text-gray-700
-                                '>
-                                    <input
-                                    value={userName}
-                                    onChange={handleChangeUserName}
-                                    placeholder='Nola Stradford'
-                                    >
-                                    </input>
-                            </h1>
-
-                        </div>
-                    </div>
-                </div>
-                <ul
-                className='
-                text-gray-600
-                mt-6
+              <input
+                value={userName}
+                onChange={handleChangeUserName}
+                onKeyDown={handleOnKeyDown}
+                onCompositionStart={handleCompositionStart}
+                onCompositionEnd={handleCompositionEnd}
+                placeholder='Nola Stradford'
+              ></input>
+            </h1>
+          </div>
+        </div>
+      </div>
+      <ul
+        className='
+        text-gray-600
+        mt-6
+        flex
+        flex-col
+        gap-y-2
+        text-sm'
+      >
+        <li>
+          <ul
+            className='
+            flex
+            items-start
+            sm:items-center
+            flex-col
+            sm:flex-row
+            justify-start
+            '
+          >
+            <li
+              className='
+                pl-3
+                sm:pl-0
+                sm:w-[128px]
+                text-sm
+                text-gray-400'
+            >
+              メールアドレス
+            </li>
+            <li
+              //   value={currentEmail}
+              className='
+                h-7
                 flex
-                flex-col
-                gap-y-2
-                text-sm'>
-                        <li>
-                            <ul
-                            className='
-                            flex
-                            items-start
-                            sm:items-center
-                            flex-col
-                            sm:flex-row
-                            justify-start
-                            '>
-                                <li className='
-                                pl-3
-                                sm:pl-0
-                                sm:w-[128px]
-                                text-sm
-                                text-gray-400'>
-                                    メールアドレス
-                                </li>
-                                <li
-                                value={currentEmail}
-                                className="
-                                h-7
-                                flex
-                                flex-1
-                                sm:min-w-[14rem]
-                                sm:w-[20rem]
-                                w-[18rem]
-                                p-4
-                                m-2
-                                "
-                                >
-                                </li>
-                            </ul>
-                        </li>
-                        <li
-                        className='
-                        min-w-[16rem]
-                        w-full
-                        pl-3
-                        sm:pl-0
-                        flex
-                        flex-col
-                        sm:flex-row
-                        justify-between'
-                        >
-                            <ul
-                            className='
-                            flex
-                            items-start
-                            sm:items-center
-                            flex-col
-                            sm:flex-row
-                            justify-start'>
-                                <li
-                                className='
-                                sm:w-[128px]
-                                text-sm
-                                text-gray-400
-                                '
-                                >
-                                 パスワード
-                                </li>
-                                <li
-                                className='
-                                sm:pl-4
-                                pl-2
-                                m-2
-                                text-gray-400
-                                tracking-widest
-                                '
-                                >
-                                ******
-                                </li>
-                            </ul>
-                            <Link
-                            href="/ResetPassword"
-                            >
-                                <button
-                                    className='
-                                    text-gray-400
-                                    '>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                        </svg>
-                                </button>
-                            </Link>
-                        </li>
-                </ul>
+                flex-1
+                sm:min-w-[14rem]
+                sm:w-[20rem]
+                w-[18rem]
+                p-4
+                m-2
+                '
+            ></li>
+          </ul>
+        </li>
+        <li
+          className='
+            min-w-[16rem]
+            w-full
+            pl-3
+            sm:pl-0
+            flex
+            flex-col
+            sm:flex-row
+            justify-between'
+        >
+          <ul
+            className='
+            flex
+            items-start
+            sm:items-center
+            flex-col
+            sm:flex-row
+            justify-start'
+          >
+            <li
+              className='
+                sm:w-[128px]
+                text-sm
+                text-gray-400
+                '
+            >
+              パスワード
+            </li>
+            <li
+              className='
+                sm:pl-4
+                pl-2
+                m-2
+                text-gray-400
+                tracking-widest
+                '
+            >
+              ******
+            </li>
+          </ul>
+          <Link href='/ResetPassword'>
+            <button
+              className='
+                text-gray-400
+                '
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-4 w-4'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth='2'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'
+                />
+              </svg>
+            </button>
+          </Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};

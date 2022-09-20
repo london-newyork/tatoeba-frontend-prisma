82d2ce33fbff3274646af4f31564fa48ae9dbcfe
YI <57102971+london-newyork@users.noreply.github.com>
2022-09-20 22:59:08 +0900

    #30 GETの実装部分を削除 - フロントでGETを実装しなくてもPUTだけでAPI側GETが機能するのでGETの実装部分を削除

--- src/components/hooks/useUserInfo.tsx
+++ src/components/hooks/useUserInfo.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { User } from '../types/types';
 import { LoginUserAtom } from '../utils/atoms/LoginUserAtom';
+import { ProfileImageAtom } from '../utils/atoms/ProfileImageAtom';
 
 export const useUserInfo = (userId: string | null) => {
   const [user, setUser] = useState<User | undefined>(undefined);
   const persistAccessToken = useRecoilValue(LoginUserAtom);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  console.log('useUserInfo after ### useState ### user', user);
-  const [avatarImagePath, setAvatarImagePath] = useState('');
+  // console.log('useUserInfo after ### useState ### user', user);
+  const setProfileImage = useSetRecoilState(ProfileImageAtom);
 
   useEffect(() => {
     setIsLoading(true);
@@ -63,36 +64,6 @@ export const useUserInfo = (userId: string | null) => {
     console.log('user userName', user?.userName); // 1週遅れ
   };
 
-  // これだとマウント時に機能するわけではないためuseEffect使いたい
-  // => 使うとエラーになる
-  const getUserProfileImage = async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/${userId}/profile_image`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
-      .then((res) => {
-        if (!res.ok) {
-          throw '画像取得エラーがおきました。';
-        }
-        return res.blob();
-      })
-      .then((blobData) => {
-        // TODO Profile avatarの<img src>に変数を入れる
-        // 直接 GCS URLをsrc指定だと使い切りタイプ?のようなので
-        // 一意のURLを作成して変数化、ただし同じURL使って再度画像を取得することはできない
-
-        // 赤いボタンを押すとblobは生成されてる
-        const newAvatarImagePath = URL.createObjectURL(blobData);
-        console.log(newAvatarImagePath);
-        setAvatarImagePath(newAvatarImagePath);
-      });
-  };
-
   const updateUserProfileImage = async (file: File) => {
     const formData = new FormData();
     formData.append('image', file);
@@ -106,10 +77,9 @@ export const useUserInfo = (userId: string | null) => {
         body: formData,
       }
     );
-    // TODO APIでGCS登録されたデータが返ってきてそれをフロントのアバターに登録？
-    // Blob: {size:}が返ってきた
-    const data = await res.blob();
-    console.log(data);
+    setProfileImage(new Date().getTime());
+    // const data = await res.blob();
+    // console.log('updated data : ', data);
   };
   return {
     user,
@@ -117,7 +87,5 @@ export const useUserInfo = (userId: string | null) => {
     isLoading,
     error,
     updateUserProfileImage,
-    getUserProfileImage,
-    avatarImagePath,
   };
 };

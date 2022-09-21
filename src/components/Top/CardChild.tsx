66caea851c8432ea106312c2630133b7baec17dc
YI <57102971+london-newyork@users.noreply.github.com>
2022-09-21 23:46:34 +0900

    #73 APIから例え一覧を参照することに成功 - ただしユーザー名をDBのTatoeに登録していなかったのでユーザー名が表示されていない。

--- src/components/Top/CardChild.tsx
+++ src/components/Top/CardChild.tsx
@@ -1,4 +1,4 @@
-import React, { VFC } from 'react';
+import React, { useEffect, useState, VFC } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { useHandleMoveToResult } from '../hooks/handleMoveToResult';
 import { Tatoe } from '../types/types';
@@ -7,9 +7,12 @@ import Image from 'next/image';
 import { useAuth } from '../hooks/useAuth';
 import { useUserInfo } from '../hooks/useUserInfo';
 import { ProfileImageAtom } from '../utils/atoms/ProfileImageAtom';
+import { useTatoe } from '../hooks/useTatoe';
+import { useRouter } from 'next/router';
+import { LoginUserAtom } from '../utils/atoms/LoginUserAtom';
 
 export const CardChild: VFC = () => {
-  const [tatoe, setTatoe] = useRecoilState<Tatoe[]>(TatoeAtom);
+  // const [tatoe, setTatoe] = useRecoilState<Tatoe[]>(TatoeAtom);
 
   const RandomColors = [
     'hover:shadow-plane_2xl_card_prime',
@@ -24,17 +27,135 @@ export const CardChild: VFC = () => {
 
   const { userId } = useAuth();
   const profileImage = useRecoilValue(ProfileImageAtom);
+  const [allUserTatoe, setAllUserTatoe] = useState([]);
 
   const { user } = useUserInfo(userId);
+  // ここから　getTatoeを使った実装　テスト
+  // 外からでも見られるように実装する => ログインユーザーの user いらない persistAccessToken いらない ?
+  useEffect(() => {
+    const main = async () => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tatoe`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const { tatoe } = await res.json();
+      console.log(tatoe);
 
-  if (!userId || !user) {
-    return null;
-  }
-  const userName = user.userName;
+      setAllUserTatoe(tatoe);
+    };
+    main();
+  }, []);
+
+  // これだとログアウトしたらリストが消える
+  // if (!userId || !user) {
+  //   return null;
+  // }
+  // const userName = user.userName;
 
   return (
     <>
-      {tatoe
+      {allUserTatoe
+        ? allUserTatoe.map((item: Tatoe) => (
+            <li
+              key={item.tId}
+              className={`
+                    px-4
+                    pt-4
+                    h-[312px]
+                    w-[280px]
+                    rounded-2xl
+                    shadow-plane_2xl_card
+                    ${shadowColor}
+                    border-[1px]
+                    border-gray-800
+                    bg-white
+                    `}
+              onClick={() =>
+                handleMoveToResult({
+                  tId: item.tId,
+                  title: item.title,
+                  shortParaphrase: item.shortParaphrase,
+                  description: item.description,
+                })
+              }
+            >
+              <ul
+                className='
+                        h-[72px]
+                        pl-1
+                        '
+              >
+                <li>
+                  <ul
+                    className='
+                    flex
+                    items-center
+                    gap-x-2
+                    pb-2
+                    '
+                  >
+                    <img
+                      src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/${userId}/profile_image?t=${profileImage}`}
+                      alt='ユーザーの画像'
+                      className='
+                      w-6
+                      h-6
+                      rounded-full
+                      object-cover'
+                    />
+                    <p
+                      className='
+                    text-xs
+                    text-gray-400
+                    '
+                    >
+                      {item.userName}が投稿
+                    </p>
+                  </ul>
+                  <h3
+                    className='
+                                text-left
+                                text-dark_green
+                                text-lg
+                                '
+                  >
+                    {item.title}
+                    <br />
+                    <span className='text-gray-500'>を例えると...</span>
+                    {item.shortParaphrase}
+                  </h3>
+                </li>
+              </ul>
+              <ul
+                className='
+                        pt-5
+                        '
+              >
+                <li
+                  className='
+                            flex
+                            flex-col
+                            items-center
+                            pt-6
+                            '
+                >
+                  <Image
+                    src='/images/illust1.png'
+                    alt='illust of Tatoeba app'
+                    width={240}
+                    height={120}
+                    objectFit='contain'
+                    quality={50}
+                    priority={true}
+                  />
+                </li>
+              </ul>
+            </li>
+          ))
+        : undefined}
+      {/* {tatoe
         ? tatoe.map((item: Tatoe) => (
             <li
               key={item.tId}
@@ -132,7 +253,7 @@ export const CardChild: VFC = () => {
               </ul>
             </li>
           ))
-        : undefined}
+        : undefined} */}
     </>
   );
 };

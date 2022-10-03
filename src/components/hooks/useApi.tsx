7759105f88aa3fcdd99b05e47566f2824ab35b3a
YI <57102971+london-newyork@users.noreply.github.com>
2022-10-03 22:44:19 +0900

    #81 結果的に今回はblobを使わず実装したため一旦コメントアウト(あとで削除)

--- src/components/hooks/useApi.tsx
+++ src/components/hooks/useApi.tsx
@@ -27,16 +27,16 @@ export const useApi = (url: string, { method }: UseApiOptions) => {
     });
 
     // 画像のとき
-    const contentType = res.headers.get('Content-Type');
-    if (method === 'GET' && contentType.match(/image/)) {
-      const imageData = await res.blob();
-      const blobUrl = URL.createObjectURL(imageData);
-      return blobUrl;
-    } else {
-      // 画像以外
-      const data = await res.json();
-      return data;
-    }
+    // const contentType = res.headers.get('Content-Type');
+    // if (method === 'GET' && contentType.match(/image/)) {
+    //   const imageData = await res.blob();
+    //   const blobUrl = URL.createObjectURL(imageData);
+    //   return blobUrl;
+    // } else {
+    // 画像以外
+    const data = await res.json();
+    return data;
+    // }
   };
 
   return { api };

71aaa4b1cc918fcf168836d59eaa93eb29e608a9
YI <57102971+london-newyork@users.noreply.github.com>
2022-10-18 14:05:24 +0900

    #94 ログインフォームに準じる共通のフォームをapplyでまとめた

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -39,147 +39,48 @@ export default function Login() {
       </Head>
       <Header />
       <LoginLayouts>
-        <section
-          className='
-            bg-gray-100
-            h-screen
-            px-2
-            md:px-0
-            mx-auto
-            flex
-            justify-center
-            '
-        >
-          <div
+        <h1 className='pt-12 login-headline'>ログイン</h1>
+        <div className='pt-14 flex flex-col gap-6'>
+          <div className='flex flex-col'>
+            <p className='login-headline-s'>メールアドレス</p>
+            <input
+              value={email}
+              className='login-input login-input-cstm
+                      '
+              onChange={handleChangeEmail}
+            />
+          </div>
+          <div className='flex flex-col'>
+            <p className='login-headline-s'>パスワード</p>
+            <input
+              value={password}
+              className='login-input login-input-cstm'
+              onChange={handleChangePassword}
+            />
+          </div>
+          <button
             className='
-                bg-white
-                px-7
-                pt-20
-                pb-7
-                rounded-3xl
-                border-[1px]
-                border-gray-800
-                min-w-[24rem]
-                md:w-[24rem]
-                h-[31.25rem]
-                flex
-                flex-col
-                items-center
-                '
-          >
-            <h1
-              className='
-                  relative
-                  text-3xl
-                  text-gray-700
-                  select-none
-                  font-normal
-                  '
-            >
-              ログイン
-            </h1>
-            <div className='pt-14 flex flex-col gap-6'>
-              <div className='flex flex-col'>
-                <p
-                  className='
-                      pr-2
-                      font-normal
-                      text-gray-600
-                      w-[128px]
-                      text-sm
-                      pb-2
-                      '
-                >
-                  メールアドレス
-                </p>
-                <input
-                  value={email}
-                  className='
-                      outline-none
-                      focus:ring-2
-                      focus:ring-offset-3
-                      focus:ring-green-100
-                      focus:ring-offset-green-50
-                      focus:border-green-100
-                      focus:placeholder-gray-300
-                      h-8
-                      py-2
-                      px-3
-                      w-[300px]
-                      border
-                      border-gray-200
-                      rounded-full
-                      '
-                  onChange={handleChangeEmail}
-                />
-              </div>
-              <div className='flex flex-col'>
-                <p
-                  className='
-                      pr-2
-                      text-gray-600
-                      w-[128px]
-                      text-sm
-                      pb-2
-                      '
-                >
-                  パスワード
-                </p>
-                <input
-                  value={password}
-                  className='
-                        shadow-sm
-                        outline-none
-                        focus:ring-2
-                        focus:ring-offset-3
-                        focus:ring-green-100
-                        focus:ring-offset-green-50
-                        focus:border-green-100
-                        focus:placeholder-gray-300
-                        h-8
-                        py-2
-                        px-3
-                        w-[300px]
-                        border
-                        border-gray-200
-                        rounded-full
-                        '
-                  onChange={handleChangePassword}
-                />
-              </div>
-              <button
-                className='
                     mt-4
                     mb-4
-                    mx-auto
-                    p-3
-                    w-full
-                    rounded-full
-                    bg-dark_green
-                    text-gray-800
-                    text-lg
-                    hover:bg-opacity-90
-                    font-normal
+                    login-btn
                     '
-                onClick={handleLogin}
-              >
-                ログイン
-              </button>
-            </div>
-            <Link href='/RegisterMember/TempRegisterMember'>
-              <p
-                className='
+            onClick={handleLogin}
+          >
+            ログイン
+          </button>
+        </div>
+        <Link href='/RegisterMember/TempRegisterMember'>
+          <p
+            className='
                   mx-auto
                   text-sm
                   text-gray-600
                   cursor-pointer
                   '
-              >
-                新規会員登録
-              </p>
-            </Link>
-          </div>
-        </section>
+          >
+            新規会員登録
+          </p>
+        </Link>
       </LoginLayouts>
     </>
   );
--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -70,169 +70,51 @@ const ResetPassword = () => {
       </Head>
       <Header />
       <LoginLayouts>
-        <section
+        <h1
           className='
-                bg-gray-100
-                h-screen
-                px-2
-                md:px-0
-                mx-auto
-                flex
-                justify-center
-                '
+              pt-8
+              login-headline
+              '
         >
-          <div
-            className='
-                    bg-white
-                    px-7
-                    pt-20
-                    pb-7
-                    rounded-3xl
-                    border-[1px]
-                    border-gray-800
-                    min-w-[24rem]
-                    md:w-[24rem]
-                    h-[31.25rem]
-                    flex
-                    flex-col
-                    items-center
-                    '
-          >
-            <h1
+          パスワード再設定
+        </h1>
+        <div className='pt-10 flex flex-col gap-6'>
+          <div className='flex flex-col'>
+            <p className='login-headline-s'>現在のパスワード</p>
+            <input
+              value={currentPassword}
+              className='login-input login-input-cstm'
+              onChange={handleChangeCurrentPassword}
+            />
+          </div>
+          <div className='flex flex-col'>
+            <p className='login-headline-s'>新パスワード</p>
+            <input
+              value={newPassword}
               className='
-                    relative
-                    text-3xl
-                    text-gray-700
-                    select-none
-                    font-normal
-                    '
-            >
-              パスワード再設定
-            </h1>
-            <div className='pt-14 flex flex-col gap-6'>
-              <div className='flex flex-col'>
-                <p
-                  className='
-                        pr-2
-                        text-gray-600
-                        w-[128px]
-                        text-sm
-                        pb-2
-                        '
-                >
-                  現在のパスワード
-                </p>
-                <input
-                  value={currentPassword}
-                  className='
-                            shadow-sm
-                            outline-none
-                            focus:ring-2
-                            focus:ring-offset-3
-                            focus:ring-green-100
-                            focus:ring-offset-green-50
-                            focus:border-green-100
-                            focus:placeholder-gray-300
-                            h-8
-                            py-2
-                            px-3
-                            w-[300px]
-                            border
-                            border-gray-200
-                            rounded-full
-                            '
-                  onChange={handleChangeCurrentPassword}
-                />
-              </div>
-              <div className='flex flex-col'>
-                <p
-                  className='
-                        pr-2
-                        text-gray-600
-                        w-[128px]
-                        text-sm
-                        pb-2
-                        '
-                >
-                  新パスワード
-                </p>
-                <input
-                  value={newPassword}
-                  className='
-                            shadow-sm
-                            outline-none
-                            focus:ring-2
-                            focus:ring-offset-3
-                            focus:ring-green-100
-                            focus:ring-offset-green-50
-                            focus:border-green-100
-                            focus:placeholder-gray-300
-                            h-8
-                            py-2
-                            px-3
-                            w-[300px]
-                            border
-                            border-gray-200
-                            rounded-full
-                            '
-                  onChange={handleChangeNewPassword}
-                />
-              </div>
-              <div className='flex flex-col'>
-                <p
-                  className='
-                        pr-2
-                        text-gray-600
-                        w-[128px]
-                        text-sm
-                        pb-2
-                        '
-                >
-                  新パスワード確認
-                </p>
-                <input
-                  value={confirmPassword}
-                  className='
-                            shadow-sm
-                            outline-none
-                            focus:ring-2
-                            focus:ring-offset-3
-                            focus:ring-green-100
-                            focus:ring-offset-green-50
-                            focus:border-green-100
-                            focus:placeholder-gray-300
-                            h-8
-                            py-2
-                            px-3
-                            w-[300px]
-                            border
-                            border-gray-200
-                            rounded-full
-                            '
-                  onChange={handleChangeConfirmPassword}
-                />
-              </div>
-              <button
-                className='
+              login-input login-input-cstm'
+              onChange={handleChangeNewPassword}
+            />
+          </div>
+          <div className='flex flex-col'>
+            <p className='login-headline-s'>新パスワード確認</p>
+            <input
+              value={confirmPassword}
+              className='login-input login-input-cstm'
+              onChange={handleChangeConfirmPassword}
+            />
+          </div>
+          <button
+            className='
                         mt-4
                         mb-4
-                        mx-auto
-                        p-3
-                        w-full
-                        rounded-full
-                        bg-dark_green
-                        text-gray-800
-                        text-lg
-                        hover:bg-opacity-90
-                        font-normal
+                        login-btn
                         '
-                onClick={handleResetPassword}
-              >
-                パスワード再設定
-              </button>
-            </div>
-          </div>
-        </section>
+            onClick={handleResetPassword}
+          >
+            パスワード再設定
+          </button>
+        </div>
       </LoginLayouts>
     </div>
   );

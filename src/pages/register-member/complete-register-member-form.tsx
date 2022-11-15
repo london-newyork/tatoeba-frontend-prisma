c4e30f0be58dacffcabdf37a8c869154ba4dd6fc
YI <57102971+london-newyork@users.noreply.github.com>
2022-11-16 00:45:17 +0900

    #94 ログイン周りリファクタの続き

--- src/pages/register-member/complete-register-member-form.tsx
+++ src/pages/register-member/complete-register-member-form.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
+import { LoginBtn } from '../../components/btn/LoginBtn';
 import { Header } from '../../components/Header/Header';
+import { HeadLine } from '../../components/HeadLine';
 import { LoginLayouts } from '../../components/Layouts/LoginLayouts';
+import { LoginInputs } from '../../components/Login/LoginInputs';
 
 const CompleteRegisterMemberForm = () => {
   const [confirmRegistrations, setConfirmRegistrations] = useState([]);
@@ -53,27 +56,15 @@ const CompleteRegisterMemberForm = () => {
       </Head>
       <Header />
       <LoginLayouts>
-        <h1 className='login-headline'>新規会員登録完了手続き</h1>
-        {/* <p className='pt-10 text-gray-400'>パスワードを入力してください</p> */}
+        <HeadLine style='login-headline' text='新規会員登録完了手続き' />
         <div className='pt-14 flex flex-col gap-6'>
-          <div className='flex flex-col'>
-            <p className='login-headline-s'>新規パスワード</p>
-            <input
-              value={password}
-              className='login-input login-input-cstm'
-              onChange={handleChangePassword}
-            />
-          </div>
-          <button
-            className='
-                    mt-4
-                    mb-4
-                    login-btn
-                    '
-            onClick={handleSendPassword}
-          >
-            登録を完了する
-          </button>
+          <LoginInputs
+            inputsTitle='新規パスワード'
+            value={password}
+            onChange={handleChangePassword}
+          />
+          {/* TODO:ログインボタン？？適切な名前に変更したい。 */}
+          <LoginBtn onClick={handleSendPassword} text='登録を完了する' />
         </div>
       </LoginLayouts>
     </div>

2a4d6425dcb606ad5d125ca3d346358a5f5cc7e2
YI <57102971+london-newyork@users.noreply.github.com>
2022-11-18 22:02:41 +0900

    #104 Gitに大文字小文字を区別させたためファイルが区別のついたファイルがGitにアップされた。

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Header } from "../../components/Header/Header";
+import { DashBoardLayouts } from "../../components/Layouts/DashBoard/DashBoardLayouts";
+import { ProfileLayouts } from "../../components/Layouts/DashBoard/ProfileLayouts";
+
+import { Profile } from "../../components/DashBoard/Profile";
+import Head from "next/head";
+
+const DashBoard = () => {
+  return (
+    <div>
+      <Head>//</Head>
+      <div>
+        <Header />
+        <DashBoardLayouts>
+          <ProfileLayouts>
+            <Profile />
+          </ProfileLayouts>
+        </DashBoardLayouts>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoard;
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
+import { Header } from "../../components/Header/Header";
+
+import { RegisterTatoeHeadline } from "../../components/Register/RegisterTatoeHeadline";
+import CreateTatoePage from "./CreateTatoePage";
+
+export default function Register() {
+  return (
+    <>
+      <Head>
+        <title>Tatoeba 例え話 登録ページ</title>
+        <link rel="favicon.ico" />
+      </Head>
+      <Header />
+      <section
+        className="
+          md:pt-[100px]
+          pt-[80px]
+          px-7
+          md:px-18
+          mx-auto
+          mb-12
+          "
+      >
+        <div
+          className="
+              frame-large
+              mx-auto
+              max-w-[1000px]
+              "
+        >
+          <RegisterTatoeHeadline />
+          <CreateTatoePage />
+        </div>
+      </section>
+    </>
+  );
+}

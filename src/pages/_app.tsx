a6dce91f362f23e93b149109dc0fabac3a9104c2
YI <57102971+london-newyork@users.noreply.github.com>
2022-11-21 22:22:37 +0900

    #101 フォント対応の続き

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,16 @@ import '../../styles/globals.css';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { AppProps } from 'next/app';
-import { Noto_Sans_JP } from '@next/font/google';
+import { Noto_Sans_JP, Zen_Kaku_Gothic_New, Inter } from '@next/font/google';
 
 const noto = Noto_Sans_JP({ subsets: ['japanese'], weight: ['300'] });
+const zen = Zen_Kaku_Gothic_New({ subsets: ['japanese'], weight: ['300'] });
+const inter = Inter({ subsets: ['latin'], weight: ['300'] });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <main className={noto.className}>
+      <main className={zen.className}>
         <Component {...pageProps} />
       </main>
     </RecoilRoot>

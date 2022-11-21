681af1e07aab1dd7980852211d898cdc5c8614ef
YI <57102971+london-newyork@users.noreply.github.com>
2022-11-21 22:47:36 +0900

    #101 サーバーサイドとクライアントサイドでのレンダリング結果の不一致で起きた ハイドレーションエラーを解消

--- src/components/Top/CardChild/CardChild.tsx
+++ src/components/Top/CardChild/CardChild.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, VFC } from "react";
-import { useRecoilValue } from "recoil";
-import { useHandleMoveToResult } from "../../hooks/handleMoveToResult";
-import { AllUserTatoe } from "../../types/types";
-import { ProfileImageAtom } from "../../utils/atoms/ProfileImageAtom";
-import { useGetUserTatoeApi } from "../../hooks/useGetUserTatoeApi";
-import { CardChildContents } from "./CardChildContents";
+import React, { useEffect, useState, VFC } from 'react';
+import { useRecoilValue } from 'recoil';
+import { useHandleMoveToResult } from '../../hooks/handleMoveToResult';
+import { AllUserTatoe } from '../../types/types';
+import { ProfileImageAtom } from '../../utils/atoms/ProfileImageAtom';
+import { useGetUserTatoeApi } from '../../hooks/useGetUserTatoeApi';
+import { CardChildContents } from './CardChildContents';
 
 export const CardChild: VFC = () => {
   const RandomColors = [
-    "hover:shadow-plane_2xl_card_prime",
-    "hover:shadow-plane_2xl_card_second",
-    "hover:shadow-plane_2xl_card_third",
-    "hover:shadow-plane_2xl_card",
+    'hover:shadow-plane_2xl_card_prime',
+    'hover:shadow-plane_2xl_card_second',
+    'hover:shadow-plane_2xl_card_third',
+    'hover:shadow-plane_2xl_card'
   ];
 
   const colors = RandomColors[Math.floor(Math.random() * RandomColors.length)];
@@ -20,22 +20,25 @@ export const CardChild: VFC = () => {
   const { getAllUserTatoe, allUserTatoe } = useGetUserTatoeApi();
   const { handleMoveToResult } = useHandleMoveToResult(allUserTatoe);
 
+  const [isClient, setIsClient] = useState(false);
+
   const profileImage = useRecoilValue(ProfileImageAtom);
 
   useEffect(() => {
     const main = async () => {
       await getAllUserTatoe();
+      setIsClient(true);
     };
     main();
   }, []);
 
   return (
     <>
-      {allUserTatoe
+      {allUserTatoe && isClient
         ? allUserTatoe.map((item: AllUserTatoe) => (
-            <li
-              key={item.tId}
-              className={`
+            <ul key={item.tId}>
+              <li
+                className={`
                     px-4
                     pt-4
                     h-[312px]
@@ -47,25 +50,26 @@ export const CardChild: VFC = () => {
                     border-gray-800
                     bg-white
                     `}
-              onClick={() =>
-                handleMoveToResult({
-                  tId: item.tId,
-                  title: item.title,
-                  shortParaphrase: item.shortParaphrase,
-                  description: item.description,
-                  userId: item.userId,
-                })
-              }
-            >
-              <CardChildContents
-                userName={item.userName}
-                profileImage={profileImage}
-                userId={item.userId}
-                title={item.title}
-                shortParaphrase={item.shortParaphrase}
-                imageUrl={item.imageUrl}
-              />
-            </li>
+                onClick={() =>
+                  handleMoveToResult({
+                    tId: item.tId,
+                    title: item.title,
+                    shortParaphrase: item.shortParaphrase,
+                    description: item.description,
+                    userId: item.userId
+                  })
+                }
+              >
+                <CardChildContents
+                  userName={item.userName}
+                  profileImage={profileImage}
+                  userId={item.userId}
+                  title={item.title}
+                  shortParaphrase={item.shortParaphrase}
+                  imageUrl={item.imageUrl}
+                />
+              </li>
+            </ul>
           ))
         : undefined}
     </>

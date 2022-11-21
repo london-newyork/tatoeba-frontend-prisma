18054d727d8e05a458f18fb123e2c1dc178bc404
YI <57102971+london-newyork@users.noreply.github.com>
2022-11-21 22:46:23 +0900

    #101 Next/fontで対応したがあまりいけてなかったので修正した。 - ただしまだ納得できていないので今後も調査する。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,12 @@ import { Noto_Sans_JP, Zen_Kaku_Gothic_New, Inter } from '@next/font/google';
 
 const noto = Noto_Sans_JP({ subsets: ['japanese'], weight: ['300'] });
 const zen = Zen_Kaku_Gothic_New({ subsets: ['japanese'], weight: ['300'] });
-const inter = Inter({ subsets: ['latin'], weight: ['300'] });
+const inter = Inter({ subsets: ['latin'], weight: ['500'] });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <main className={zen.className}>
+      <main className={noto.className}>
         <Component {...pageProps} />
       </main>
     </RecoilRoot>

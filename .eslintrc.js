c0889f693b9bfaf2488a811cc53101974cd91ec4
YI <57102971+london-newyork@users.noreply.github.com>
2023-01-13 21:40:04 +0900

    #100 Storybookをインストール

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,40 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true,
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended', 'next/core-web-vitals', 'plugin:tailwindcss/recommended', 'plugin:jest/recommended', 'prettier', 'plugin:storybook/recommended'],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-        'next/core-web-vitals',
-        'plugin:tailwindcss/recommended',
-        'plugin:jest/recommended',
-        'prettier',
-    ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'semi': ['warn', 'always'],
-        'quotes': ['error', 'single'],
-        'no-undef': 'error',
-        'react/prop-types': 'off',
-        'react/jsx-props-no-spreading': 'off',
-    },
-    'globals': {
-        React: true,
-        google: true,
-        mount: true,
-        mountWithRouter: true,
-        shallow: true,
-        shallowWithRouter: true,
-        context: true,
-        expect: true,
-        jsdom: true,
-        JSX: true,
-    },
-    'settings': {
-        'react': {
-            'version': 'detect'
-        },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': ['react'],
+  'rules': {
+    'semi': ['warn', 'always'],
+    'quotes': ['error', 'single'],
+    'no-undef': 'error',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off'
+  },
+  'globals': {
+    React: true,
+    google: true,
+    mount: true,
+    mountWithRouter: true,
+    shallow: true,
+    shallowWithRouter: true,
+    context: true,
+    expect: true,
+    jsdom: true,
+    JSX: true
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
     }
+  }
 };
